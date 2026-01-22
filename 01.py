6fd40a5d78ff5b8f26adc38390b4e972d76d0ee4
90sasha09 <sashaglod310@gmail.com>
2026-01-22 13:46:47 +0200

    next lesson

---
+++ 01.py
@@ -0,0 +1,9 @@
+a = 10
+b = "bob"
+c = 3,14 * a
+d = false
+e = [1, 2, 3]
+
+number_of_items = 10
+name = "Bob"
+price_per_item = 3,14
---
+++ 02.py
@@ -0,0 +1,15 @@
+a = 10
+
+b = a
+
+
+a = "String"
+
+
+b = 33
+
+d = 33
+
+print(id(b))
+print(id(d))
+
